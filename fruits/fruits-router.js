6bbea2512df01fd35cf703ebf62f358e386f4c68
Josh Riddle <riddlej9879@yahoo.com>
2020-08-20 19:55:45 -0500

    1st migration

--- fruits/fruits-router.js
+++ fruits/fruits-router.js
@@ -1,44 +1,37 @@
-const express = require("express")
-const knex = require("knex")
+const express = require("express");
+const knex = require("knex");
+const db = require("../data/config");
 
-const db = knex({
-	client: "sqlite3",
-	useNullAsDefault: true,
-	connection: {
-		filename: "./data/produce.db3",
-	},
-})
-
-const router = express.Router()
+const router = express.Router();
 
 router.get("/fruits", async (req, res, next) => {
-	try {
-		res.json(await db("fruits"))
-	} catch(err) {
-		next(err)
-	}
-})
+  try {
+    res.json(await db("fruits"));
+  } catch (err) {
+    next(err);
+  }
+});
 
 router.get("/fruits/:id", async (req, res, next) => {
-	try {
-		const { id } = req.params
-		const fruit = await db("fruits").where({ id }).first()
-		
-		res.json(fruit)
-	} catch(err) {
-		next(err)
-	}
-})
+  try {
+    const { id } = req.params;
+    const fruit = await db("fruits").where({ id }).first();
+
+    res.json(fruit);
+  } catch (err) {
+    next(err);
+  }
+});
 
 router.post("/fruits", async (req, res, next) => {
-	try {
-		const [id] = await db("fruits").insert(req.body)
-		const newFruit = await db("fruits").where({ id }).first()
+  try {
+    const [id] = await db("fruits").insert(req.body);
+    const newFruit = await db("fruits").where({ id }).first();
 
-		res.status(201).json(newFruit)
-	} catch(err) {
-		next(err)
-	}
-})
+    res.status(201).json(newFruit);
+  } catch (err) {
+    next(err);
+  }
+});
 
-module.exports = router
+module.exports = router;
--- welcome/welcome-router.js
+++ welcome/welcome-router.js
@@ -1,11 +1,11 @@
-const express = require("express")
+const express = require("express");
 
-const router = express.Router()
+const router = express.Router();
 
 router.get("/", (req, res, next) => {
-	res.json({
-		message: "Welcome",
-	})
-})
+  res.json({
+    message: "Welcome",
+  });
+});
 
-module.exports = router
+module.exports = router;
